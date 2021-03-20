21f65df07a4df9d22b124e9de9edc590fa877bc5
vagamerous <74257480+vagamerous@users.noreply.github.com>
2021-03-19 21:07:45 -0300

    Update BubbleSort.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,6 +1,9 @@
-def bubble_sort(lista):
+# Esse programa simples, mas poderoso, é um ordenador de lista, que ordena a lista em ordem crescente. Sua aplicação pode ser muito útil em diversos tipos de negócios diferentes.  
+# This simple but powerful program is a list sorter, which sorts the list in ascending order. Its application can be very useful in several different types of businesses. 
 
-    fim = len(lista)
+def bubble_sort(lista):  # definindo a função, colocando como parâmetro a lista a ser ordenada; defining the function, placing the list to be sorted as a parameter. 
+
+    fim = len(lista) # colocando em uma variável o tamanho da lista; placing the list size in a variable; 
 
     for i in range(fim-1, 0, -1):
         for j in range(i):
